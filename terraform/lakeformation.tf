842cdd9672a496320d48c6e54dc816160c6b7ef4
ShigeruOda <seq.drug@gmail.com>
2025-07-05 23:20:20 +0900

    process-01

--- terraform/lakeformation.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_lakeformation_permissions" "firehose_role" {
-  principal = aws_iam_role.firehose_role.arn
-  permissions = ["SELECT", "DESCRIBE"]
-
-  table {
-    database_name = "builders_flash_logs"
-    name          = "api_logs_parquet"
-  }
-}
